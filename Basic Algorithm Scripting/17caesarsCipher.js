ba33c72a26d22492d4efce97af4372af6d8a7240
andrejarboe <andrejarboe@gmail.com>
2017-06-14 13:35:14 -0400

    add local files

---
+++ Basic Algorithm Scripting/17caesarsCipher.js
@@ -0,0 +1,20 @@
+function rot13(str) { // LBH QVQ VG!
+  str = str.split('').map().join('');
+  return str;
+}
+
+function cipher(input){
+  var symbolRegex = /[^a-zA-z]/g;
+  if(stmbolRegex.test(leter)){
+    return leter;
+  }
+
+  var codeAscii = input.charCodeAt(0);
+
+  if(){
+
+  }
+}
+
+// Change the inputs below to test
+rot13("SERR PBQR PNZC");
